bc76590b41ad1575ef1ed11113e262caa09ec5e9
Didia Aristote <thefuture2092@gmail.com>
2016-07-28 00:56:27 -0400

    Initial setup completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import App from './App'
+import rootReducer from './reducers'
+import './index.scss'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by didia on 16-07-26.
+ */
+
+const RootReducer = (state, action) => {
+    return state;
+}
+
+export default RootReducer;
